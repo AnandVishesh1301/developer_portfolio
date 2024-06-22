c95c68b1f46c652a9052fa6825cdae33bae9f5fc
Vishesh <anandvh@mail.uc.edu>
2024-06-22 02:59:19 -0400

    feat: :sparkles: Implemented the TechStack with 3D Balls
    Used the icosahedronGeometry to create mesh 3D balls to indicate my proficiency Techstack

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,7 @@
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import CanvasLoader from '../Loader';
+import { CanvasLoader } from '../Loader';
 
 
 const Computers = ({ isMobile }) => {
