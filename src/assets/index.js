29ce947b5914ef4e7cf829639e6be38cb12fc14d
Vishesh <anandvh@mail.uc.edu>
2025-01-02 01:31:31 -0500

    Revert "feat: :construction: Added fitPlant to the Project Section"
    This reverts commit 09ecf4a53171fea97c9423a4cb1d31c2f38ee2e2.

--- src/assets/index.js
+++ src/assets/index.js
@@ -27,7 +27,6 @@ import GVF from "./GVF.png";
 import toh from "./toh.png";
 import dev_port from "./dev-portfolio-png-fw.png";
 import justhurry from "./justhurry.png";
-import fitPlant from "./fit-plant.png";
 
 
 import kv from "./company/kv.png";
@@ -85,6 +84,5 @@ export {
   liam,
   clark,
   jeremy,
-  justhurry,
-  fitPlant
+  justhurry
 };
