8f4f2cbad72d758651b53d8d1682050c726b8c93
Vishesh <anandvh@mail.uc.edu>
2025-12-02 06:23:36 -0500

    Tweak hero tagline to emphasize curiosity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>{`Hi, I'm`}  <span className='text-[#915eff]'>Vishesh</span></h1>
           <p className={`${styles.introsubheading} mt-2 text-white-100`}>
-            Driven by innovation, leadership,
+            Driven by curiosity, leadership,
             and a passion for <br className='sm:block hidden' />
             impactful technology.
           </p>
